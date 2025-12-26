5273f820cb4d45fd8b7c43d2ef625f520324ec49
wwan13 <wwan13@naver.com>
2025-12-26 18:07:15 +0900

    fix : 기본 빈 설정 수정

---
+++ moamoa-infra/infra-redis/src/main/kotlin/server/config/CacheObjectMapperConfig.kt
@@ -0,0 +1,15 @@
+package server.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CacheObjectMapperConfig() {
+
+    @Bean
+    @ConditionalOnMissingBean(ObjectMapper::class)
+    fun objectMapper() = jacksonObjectMapper()
+}
