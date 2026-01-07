8c23133eace49188993a341e91aa43c07ec2aae7
wwan13 <wwan13@naver.com>
2026-01-07 16:40:23 +0900

    refactor(server) : infra-security 모듈 접근 제어자 정리

--- moamoa-infra/infra-security/src/main/kotlin/server/config/SecurityProperties.kt
+++ moamoa-infra/infra-security/src/main/kotlin/server/config/SecurityProperties.kt
@@ -3,6 +3,6 @@ package server.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "security")
-data class SecurityProperties(
+internal data class SecurityProperties(
     val secretKey: String
 )
