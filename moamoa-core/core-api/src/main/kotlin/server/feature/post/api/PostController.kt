53ef827a16076ba3d91d908c8afb533507bc81a0
wwan13 <wwan13@naver.com>
2026-01-09 23:27:32 +0900

    feat(server) : 북마크한 게시글만 조회하는 API

--- moamoa-core/core-api/src/main/kotlin/server/feature/post/api/PostController.kt
+++ moamoa-core/core-api/src/main/kotlin/server/feature/post/api/PostController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import server.feature.post.command.application.IncreaseViewCountResult
 import server.feature.post.command.application.PostService
+import server.feature.post.query.BookmarkPostQueryService
 import server.feature.post.query.PostList
 import server.feature.post.query.PostQueryConditions
 import server.feature.post.query.PostQueryService
@@ -19,7 +20,8 @@ class PostController(
     private val postService: PostService,
     private val postQueryService: PostQueryService,
     private val techBlogPostQueryService: TechBlogPostQueryService,
-    private val subscribingPostQueryService: SubscribingPostQueryService
+    private val subscribingPostQueryService: SubscribingPostQueryService,
+    private val bookmarkPostQueryService: BookmarkPostQueryService,
 ) {
 
     @PostMapping("/{postId}/view")
@@ -57,6 +59,16 @@ class PostController(
         @RequestPassport passport: Passport
     ): ResponseEntity<PostList> {
         val response = subscribingPostQueryService.findAllByConditions(conditions, passport)
+
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/bookmark")
+    suspend fun findAllByBookmarkConditions(
+        conditions: PostQueryConditions,
+        @RequestPassport passport: Passport
+    ): ResponseEntity<PostList> {
+        val response = bookmarkPostQueryService.findAllByConditions(conditions, passport)
 
         return ResponseEntity.ok(response)
     }
