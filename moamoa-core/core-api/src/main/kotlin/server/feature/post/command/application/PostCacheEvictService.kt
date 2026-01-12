4b1cb65bf3cc886e9da218574d3bb451dd346632
wwan13 <wwan13@naver.com>
2026-01-12 15:32:38 +0900

    feat(server) : 북마크, 구독 생성/삭제 시 캐시 무효화

---
+++ moamoa-core/core-api/src/main/kotlin/server/feature/post/command/application/PostCacheEvictService.kt
@@ -0,0 +1,45 @@
+package server.feature.post.command.application
+
+import org.springframework.stereotype.Service
+import server.feature.post.query.PostSummary
+import server.feature.postbookmark.domain.PostBookmarkCreatedEvent
+import server.feature.postbookmark.domain.PostBookmarkRemovedEvent
+import server.feature.techblog.command.domain.TechBlogSubscribeCreatedEvent
+import server.feature.techblog.command.domain.TechBlogSubscribeRemovedEvent
+import server.infra.cache.BookmarkedPostListCache
+import server.infra.cache.SubscribedPostListCache
+import server.messaging.EventHandler
+import server.messaging.StreamDefinition
+import server.messaging.handleEvent
+
+@Service
+class PostCacheEvictService(
+    private val cacheHandlingStream: StreamDefinition,
+    private val subscribedPostListCache: SubscribedPostListCache,
+    private val bookmarkedPostListCache: BookmarkedPostListCache
+) {
+
+    @EventHandler
+    fun subscriptionCreatedPostCacheEvict() =
+        handleEvent<TechBlogSubscribeCreatedEvent>(cacheHandlingStream) { event ->
+            subscribedPostListCache.evictAll(event.memberId)
+        }
+
+    @EventHandler
+    fun subscriptionRemovedPostCacheEvict() =
+        handleEvent<TechBlogSubscribeRemovedEvent>(cacheHandlingStream) { event ->
+            subscribedPostListCache.evictAll(event.memberId)
+        }
+
+    @EventHandler
+    fun bookmarkCreatedPostCacheEvict() =
+        handleEvent<PostBookmarkCreatedEvent>(cacheHandlingStream) { event ->
+            bookmarkedPostListCache.evictAll(event.memberId)
+        }
+
+    @EventHandler
+    fun bookmarkRemovedPostCacheEvict() =
+        handleEvent<PostBookmarkRemovedEvent>(cacheHandlingStream) { event ->
+            bookmarkedPostListCache.evictAll(event.memberId)
+        }
+}
