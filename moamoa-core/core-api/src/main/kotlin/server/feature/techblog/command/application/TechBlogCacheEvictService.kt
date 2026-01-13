a84ca0da0f6ced6aa73ec1f4ed09e7c3706c2ebe
wwan13 <wwan13@naver.com>
2026-01-14 01:36:56 +0900

    feat(server) : 구독 요청/해지 이벤트 시 구독 캐시 핸들링

--- moamoa-core/core-api/src/main/kotlin/server/feature/techblog/command/application/TechBlogCacheEvictService.kt
+++ moamoa-core/core-api/src/main/kotlin/server/feature/techblog/command/application/TechBlogCacheEvictService.kt
@@ -3,6 +3,7 @@ package server.feature.techblog.command.application
 import org.springframework.stereotype.Service
 import server.feature.techblog.command.domain.TechBlogSubscribeCreatedEvent
 import server.feature.techblog.command.domain.TechBlogSubscribeRemovedEvent
+import server.infra.cache.TechBlogSubscriptionCache
 import server.infra.cache.TechBlogSummaryCache
 import server.messaging.EventHandler
 import server.messaging.StreamDefinition
@@ -11,18 +12,21 @@ import server.messaging.handleEvent
 @Service
 class TechBlogCacheEvictService(
     private val techBlogCacheHandlingStream: StreamDefinition,
-    private val techBlogSummaryCache: TechBlogSummaryCache
+    private val techBlogSummaryCache: TechBlogSummaryCache,
+    private val techBlogSubscriptionCache: TechBlogSubscriptionCache
 ) {
 
     @EventHandler
     fun subscriptionCreatedTechBlogCacheEvict() =
         handleEvent<TechBlogSubscribeCreatedEvent>(techBlogCacheHandlingStream) { event ->
             techBlogSummaryCache.evict(event.techBlogId)
+            techBlogSubscriptionCache.evictAll(event.memberId)
         }
 
     @EventHandler
     fun subscriptionRemovedTechBlogCacheEvict() =
         handleEvent<TechBlogSubscribeRemovedEvent>(techBlogCacheHandlingStream) { event ->
             techBlogSummaryCache.evict(event.techBlogId)
+            techBlogSubscriptionCache.evictAll(event.memberId)
         }
 }
