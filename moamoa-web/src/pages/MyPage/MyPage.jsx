d900061d0e88d263b069a7b8aad17616233f0fb9
wwan13 <wwan13@naver.com>
2026-01-16 23:19:08 +0900

    feat(front) : 마이페이지 퍼블리싱

--- moamoa-web/src/pages/MyPage/MyPage.jsx
+++ moamoa-web/src/pages/MyPage/MyPage.jsx
@@ -4,6 +4,9 @@ import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import GlobalSpinner from "../../components/GlobalSpinner/GlobalSpinner.jsx";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import NoteIcon from '@mui/icons-material/Note';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 export default function MyPage() {
     const { isLoggedIn, logout, isLogoutLoading } = useAuth()
@@ -18,6 +21,32 @@ export default function MyPage() {
     return (
         <div className={styles.wrap}>
             { isLogoutLoading && <GlobalSpinner /> }
+            <div>
+                <p className={styles.title}>ddangddo0511</p>
+                <div className={styles.stats}>
+                    <div className={styles.stat}>
+                        <div className={styles.statTitle}>
+                            <span className={styles.statLabel}>구독</span>
+                            <LocalOfferIcon
+                                className={styles.statIcon}
+                                sx={{fontSize: 26, color: "#3B4953", fontWeight: 800}}
+                            />
+                        </div>
+                        <span className={styles.statValue}>8</span>
+                    </div>
+                    <div className={styles.stat}>
+                        <div className={styles.statTitle}>
+                            <span className={styles.statLabel}>북마크</span>
+                            <BookmarkIcon
+                                className={styles.statIcon}
+                                sx={{fontSize: 26, color: "#90AB8B", fontWeight: 800}}
+                            />
+                        </div>
+                        <span className={styles.statValue}>8</span>
+                    </div>
+                </div>
+            </div>
+
             <section className={styles.section}>
                 <p className={styles.sectionTitle}>활동</p>
                 <div className={styles.buttons}>
