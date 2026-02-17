24bcaa42f2e5f2a0babafa659cd55e77d1f2c7e4
wwan13 <wwan13@naver.com>
2026-02-17 20:06:18 +0900

    chore(server) : 캐시 중계 모듈 infra-cache

---
+++ moamoa-infra/infra-cache/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation(project(":moamoa-core:core-shared"))
+    implementation(project(":moamoa-infra:infra-redis"))
+    implementation(project(":moamoa-infra:infra-caffeine"))
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    testImplementation(project(":moamoa-support:support-test"))
+}
