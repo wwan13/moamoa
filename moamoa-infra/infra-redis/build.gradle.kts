394992a0b701b4697247c2bf50c21400643d8c36
wwan13 <wwan13@naver.com>
2026-02-20 19:52:28 +0900

    feat(server) : 로깅 의존성 추가

--- moamoa-infra/infra-redis/build.gradle.kts
+++ moamoa-infra/infra-redis/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":moamoa-core:core-shared"))
+    implementation(project(":moamoa-support:support-logging"))
 
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 
