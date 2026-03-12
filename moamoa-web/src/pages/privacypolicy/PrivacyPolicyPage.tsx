4a65a21670f3e9c73017d57dce53dbc68eb50a12
wwan13 <wwan13@naver.com>
2026-03-12 16:25:58 +0900

    fix(front) : 개인정보 처리방침 페이지 진입 시 스크롤 맨 위로

--- moamoa-web/src/pages/privacypolicy/PrivacyPolicyPage.tsx
+++ moamoa-web/src/pages/privacypolicy/PrivacyPolicyPage.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react"
 import policy from "./content/PrivacyPolicy.md?raw"
 import styles from "./PrivacyPolicyPage.module.css"
 import Markdown from "../../components/markdown/Markdown.tsx";
 
 const PrivacyPolicyPage = () => {
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: "auto" })
+    }, [])
+
     return (
         <div className={styles.body}>
             <Markdown>{policy}</Markdown>
