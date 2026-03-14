67e886cfb0406b7eedd4922559b54c6e7cf3eb6f
wwan13 <wwan13@naver.com>
2026-03-14 20:09:50 +0900

    fix(server) : batch datasource가 log datasource를 읽는 오류

--- moamoa-core/core-batch/src/main/kotlin/server/batch/config/BatchDatasourceConfig.kt
+++ moamoa-core/core-batch/src/main/kotlin/server/batch/config/BatchDatasourceConfig.kt
@@ -3,6 +3,10 @@ package server.batch.config
 import org.springframework.boot.autoconfigure.batch.BatchDataSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 import javax.sql.DataSource
 
@@ -21,4 +25,16 @@ internal class BatchDatasourceConfig(
             setPassword(props.password)
         }
     }
+
+    @Primary
+    @Bean(name = ["jdbcTemplate"])
+    fun jdbcTemplate(
+        @Qualifier("dataSource") dataSource: DataSource
+    ): JdbcTemplate = JdbcTemplate(dataSource)
+
+    @Primary
+    @Bean(name = ["namedParameterJdbcTemplate"])
+    fun namedParameterJdbcTemplate(
+        @Qualifier("dataSource") dataSource: DataSource
+    ): NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(dataSource)
 }
